4f87bfdf7cac4512e392e4029a23ec804a60c76e
LAPTOP-RSH17G7M\LENOVO <quanghuy180623@gmail.com>
2024-04-26 02:02:05 +0700

    fe with pages category country episode movie and watch

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\IndexController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'home'])->name('homepage');
+Route::get('/category', [IndexController::class, 'category'])->name('category');
+Route::get('country', [IndexController::class, 'country'])->name('country');
+Route::get('episode', [IndexController::class, 'episode'])->name('episode');
+Route::get('movie', [IndexController::class, 'movie'])->name('movie');
+Route::get('watch', [IndexController::class, 'watch'])->name('watch');
 
 Auth::routes();
 
